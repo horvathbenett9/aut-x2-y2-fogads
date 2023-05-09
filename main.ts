5f7f7762e77794a4d62cc6ca745135ce31b11e47
horvathbenett9 <75091388+horvathbenett9@users.noreply.github.com>
2023-05-09 20:10:07 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    if (x > 5 && x < -5) {
+        if (y > 5 && y < -5) {
+        	
+        } else {
+            RingbitCar.brake()
+        }
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == "x2") {
+        x = value
+        if (Math.abs(x) <= 20) {
+            x = 0
+        }
+    }
+    if (name == "y2") {
+        y = value
+        if (Math.abs(y) <= 20) {
+            y = 0
+        }
+    }
+})
+let y = 0
+let x = 0
+led.setBrightness(25)
+radio.setGroup(1)
+RingbitCar.init_wheel(AnalogPin.P0, AnalogPin.P1)
+basic.forever(function () {
+    RingbitCar.freestyle(x + y, y - x)
+})
